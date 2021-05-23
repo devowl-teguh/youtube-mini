4c2aebe31e4771d0c793efb2d27cb5bfc0dd1d4c
Teguh <teguh@member.id>
2021-05-23 14:56:24 +0700

    Init

---
+++ youtube-mini/youtube_miniApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_miniApp.swift
+//  youtube-mini
+//
+//  Created by Member id on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct youtube_miniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
